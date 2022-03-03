42400e0b3c1f77a0f4ed63cd8cb54a9ca8e467ea
werkla <klaus.werner@digitalwert.de>
2022-03-03 15:13:51 +0100

    first commit

---
+++ src/components/button/button.jsx
@@ -0,0 +1,7 @@
+import './button.scss';
+
+const Button = () => {
+    return <button className='btn'>Click me</button>
+}
+
+export default Button;
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,10 @@
+import Button from './../../components/button/button';
+
+const Home = () => {
+    return <section className="home">
+        <h2>Home</h2>
+        <Button/>
+    </section>
+}
+
+export default Home;
